7d73faec988a966ae808f513602e236e4872fecb
HeliXZz <helixzz@helixzz.com>
2017-05-21 00:48:32 +0800

    1. 修复 provider 未开启或无效导致的程序 crash 问题； 2. 重写定位部分逻辑。

--- app/src/main/java/com/example/shinianer/goodweather/util/LocationHelper.java
+++ app/src/main/java/com/example/shinianer/goodweather/util/LocationHelper.java
@@ -14,78 +14,53 @@ import android.renderscript.Double2;
 import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
 
+import com.example.shinianer.goodweather.MainActivity;
 import com.google.gson.Gson;
 
 /**
  * Created by Shinianer on 2017/5/15.
  */
 
-public class LocationHelper {
+public class LocationHelper extends MainActivity {
     Context mContext;
     private LocationManager locationManager;
+
     public LocationHelper(Context mContext) {
         this.mContext = mContext;
     }
 
     public Location getLocation() {
         locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        // 获得最好的定位效果
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        criteria.setAltitudeRequired(false);
-        criteria.setBearingRequired(false);
-        // 使用省电模式
-        criteria.setPowerRequirement(Criteria.POWER_LOW);
-        // 查询最合适的 Provider
-        String provider = locationManager.getBestProvider(criteria, true);
-        // 或者直接使用网络定位
-        //String provider = LocationManager.NETWORK_PROVIDER;
 
-        if (locationManager.isProviderEnabled(provider)) {
-            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                // return TODO;
-                Toast.makeText(mContext, "未能调用定位。请检查定位权限是否开启，或是否处于飞行模式。", Toast.LENGTH_LONG).show();
-            }
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (location != null) {
-                storeLastKnownLocation(location);
-                return location;
-            } else {
-                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                if (location != null) {
-                    storeLastKnownLocation(location);
-                    return location;
-                } else {
-                    location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-                    if (location != null) {
-                        storeLastKnownLocation(location);
-                        return location;
-                    } else {
-                        Toast.makeText(mContext, "获取实时位置失败。", Toast.LENGTH_LONG).show();
-                        return null;
-                    }
-                }
-            }
-        } else {
-            Toast.makeText(mContext, "系统定位功能未开启。", Toast.LENGTH_LONG).show();
-            return null;
+        // 检查 App 是否具备定位权限。如果未具备，提示用户，并尝试索取权限。
+        if (ActivityCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(mContext, "请检查定位权限是否开启，或是否处于飞行模式。", Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission_group.LOCATION}, 124);
         }
 
+        // 枚举三种 Location Provider，从 GPS 开始逐个检查是否开启，并尝试获取位置。
+        // 如果没有开启或获取不到位置，则继续尝试下一个，直到全部试完，发出提示并返回空值。
+        String[] providers = new String[]{LocationManager.GPS_PROVIDER,
+                LocationManager.NETWORK_PROVIDER, LocationManager.PASSIVE_PROVIDER};
+        for (String provider : providers) {
+            if (locationManager.isProviderEnabled(provider)) {
+                Location location = locationManager.getLastKnownLocation(provider);
+                if (location != null) {
+                    // 成功获取到位置后，存入 SharedPreferences。
+                    storeLastKnownLocation(location);
+                    return location;
+                }
+            }
+        }
+        Toast.makeText(mContext, "获取实时位置失败。", Toast.LENGTH_LONG).show();
+        return null;
     }
 
     public void storeLastKnownLocation(Location location) {
-//        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(mContext).edit();
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
-//        String jsonLocation = new Gson().toJson(location);
-//        editor.putString("mLastLocation", jsonLocation);
-//        editor.apply();
         if (location == null) {
         } else {
             sharedPreferences.edit().putString("LOCATION_LAT", String.valueOf(location.getLatitude())).apply();
@@ -102,25 +77,14 @@ public class LocationHelper {
         } else {
             SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
             Location storedLocation = null;
-//            String strLastLocation = sharedPreferences.getString("mLastLocation", null);
-//            if (strLastLocation != null) {
-//                if (!strLastLocation.equals(""))
-//                    storedLocation = new Gson().fromJson(strLastLocation, Location.class);
-//            }
-//            if (storedLocation != null) {
-//                Toast.makeText(mContext, "已读取之前保存的定位信息。", Toast.LENGTH_LONG).show();
-//                return storedLocation;
-//            } else {
-//                Toast.makeText(mContext, "未能读取之前保存的定位信息。", Toast.LENGTH_LONG).show();
-//                return null;
-//            }
             String lat = sharedPreferences.getString("LOCATION_LAT", null);
             String lon = sharedPreferences.getString("LOCATION_LON", null);
-            if (lat != null && lon != null) {
-                String provider = sharedPreferences.getString("LOCATION_PROVIDER", null);
-                storedLocation = new Location(provider);
+            String provider = sharedPreferences.getString("LOCATION_PROVIDER", null);
+            if (lat != null && lon != null && provider != null) {
+                storedLocation = new Location(LocationManager.PASSIVE_PROVIDER);
                 storedLocation.setLatitude(Double.parseDouble(lat));
                 storedLocation.setLongitude(Double.parseDouble(lon));
+                Toast.makeText(mContext, "已读取之前保存的定位信息。", Toast.LENGTH_LONG).show();
             }
             return storedLocation;
         }
@@ -128,10 +92,17 @@ public class LocationHelper {
 
     public void addLocationListener(LocationListener listener) {
         locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(mContext, "未能调用定位。请检查定位权限是否开启，或是否处于飞行模式。", Toast.LENGTH_LONG).show();
+
+        // 检查 App 是否具备定位权限。如果未具备，提示用户，并尝试索取权限。
+        if (ActivityCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(mContext, "请检查定位权限是否开启，或是否处于飞行模式。", Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission_group.LOCATION}, 124);
             return;
         }
+
         Criteria criteria = new Criteria();
         // 获得最好的定位效果
         criteria.setAccuracy(Criteria.ACCURACY_FINE);
@@ -141,15 +112,28 @@ public class LocationHelper {
         criteria.setPowerRequirement(Criteria.POWER_LOW);
         // 查询最合适的 Provider
         String provider = locationManager.getBestProvider(criteria, true);
-        locationManager.requestLocationUpdates(provider, 60000, 1, listener);
+        if (provider != null) {
+            locationManager.requestLocationUpdates(provider, 60000, 1, listener);
+        }
+        else {
+            Toast.makeText(mContext, "请检查定位权限是否开启，或是否处于飞行模式。", Toast.LENGTH_LONG).show();
+        }
+
     }
 
     public void removeLocationListener(LocationListener listener) {
         locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(mContext, "未能调用定位。请检查定位权限是否开启，或是否处于飞行模式。", Toast.LENGTH_LONG).show();
+
+        // 检查 App 是否具备定位权限。如果未具备，提示用户，并尝试索取权限。
+        if (ActivityCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(mContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(mContext, "请检查定位权限是否开启，或是否处于飞行模式。", Toast.LENGTH_LONG).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission_group.LOCATION}, 124);
             return;
         }
+
         locationManager.removeUpdates(listener);
     }
 
@@ -157,6 +141,7 @@ public class LocationHelper {
         LocationListener listener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
+                // 当定位信息更新时
                 storeLastKnownLocation(location);
                 removeLocationListener(this);
             }
